4b64cbb46ddedc3a3f267c1ad7239785de9854c8
unknown <sifatniloy18@gmail.com>
2022-11-12 22:58:39 -0600

    config files added

--- src/Pages/Introduction/Introduction.js
+++ src/Pages/Introduction/Introduction.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Introduction = () => {
     return (
         <div>
-            <h2 className='text-4xl text-center bg-blue-700'>This is Sifat's site</h2>
+            <h2 className='text-4xl text-center text-blue-700 mt-10'>This is Sifat's site</h2>
         </div>
     );
 };
