bedd41fd7a025b1b77eecc3c26d999c7af9d53a5
unknown <sifatniloy18@gmail.com>
2022-11-12 22:35:09 -0600

    this is my own site

---
+++ src/Pages/Introduction/Introduction.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Introduction = () => {
+    return (
+        <div>
+            <h2>This is Sifat's site</h2>
+        </div>
+    );
+};
+
+export default Introduction;
